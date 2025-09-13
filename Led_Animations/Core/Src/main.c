f61052a67a295291f79f557d9f7fafe70c19cb2c
Hoàng Anh Duy <230195952+duyhoangbk23@users.noreply.github.com>
2025-09-14 01:09:34 +0700

    Add Proteus project files

--- Led_Animations/Core/Src/main.c
+++ Led_Animations/Core/Src/main.c
@@ -93,7 +93,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	HAL_GPIO_TogglePin ( LED_RED_GPIO_Port , LED_RED_Pin );
+	HAL_Delay (1000) ;
+
     /* USER CODE END WHILE */
+
 
     /* USER CODE BEGIN 3 */
   }
